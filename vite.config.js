3b128514081894014b9052a4e8122c565acbdecb
vipul khanvilkar <110216542+vipulkhanvilkarr@users.noreply.github.com>
2025-03-28 15:15:49 +0530

    final navbar

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: true, // Enables access via network IP
     port: 3000, // You can specify the port you want
   },
-})
+});
